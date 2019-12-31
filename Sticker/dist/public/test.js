1b77a3c79e37b634cbd9bdae5572002557c0568e
TitiGil <34870117+TitiGil@users.noreply.github.com>
2019-12-31 19:49:48 +0430

    Version 1.1

---
+++ Sticker/dist/public/test.js
@@ -0,0 +1,13 @@
+"use strict";
+
+var _React = React,
+    Component = _React.Component;
+var _ReactDOM = ReactDOM,
+    render = _ReactDOM.render;
+
+
+render(React.createElement(
+        "h1",
+        { id: "App-header", className: "checker", style: { backgroundColor: 'red' } },
+        "This is my first react app Tester!!"
+), document.getElementById("react-container"));
---
+++ Sticker/src/lib.js
@@ -0,0 +1,12 @@
+import React,{component} from 'react'  
+import text from './title.json'
+import './style.scss'
+
+export const  hello =(
+    <h1 id='"title' className="helloCommand" > {text.hello} </h1>
+);
+
+
+export const  goodBye =(
+    <h1 id='"title' className="goodByeCommand" > {text.goodBye} </h1>
+);
